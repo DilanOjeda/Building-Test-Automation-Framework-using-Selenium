4e03577ceed45ec91e94e7b332f3c1410eb2febd
DilanOjeda <dilanojeda.engineer@gmail.com>
2022-09-22 22:45:15 -0400

    task 3 - Writing wrapper methods to perform user actions

---
+++ CommonLibs/Implementation/CommonElement.cs
@@ -0,0 +1,15 @@
+using System;
+using OpenQA.Selenium;
+
+namespace CommonLibs.Implementation
+{
+    public class CommonElement
+    {
+        public void ClickElement(IWebElement element) => element.Click();
+        public void ClearText(IWebElement element) => element.Clear();
+        public void SetText(IWebElement element, string text) => element.SendKeys(text);
+        public bool IsElementDisplayed(IWebElement element) => element.Displayed;
+        public bool IsElementSelected(IWebElement element) => element.Selected;
+        public bool IsElementEnabled(IWebElement element) => element.Enabled;
+    }
+}
