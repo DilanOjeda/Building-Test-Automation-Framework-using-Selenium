0994d2aed35a159491e76af6197135e646f357bd
DilanOjeda <dilanojeda.engineer@gmail.com>
2022-09-26 23:29:37 -0400

    Reading configuration from appsettings json

--- Guru99Tests/tests/BaseTests.cs
+++ Guru99Tests/tests/BaseTests.cs
@@ -1,9 +1,11 @@
+using System.IO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CommonLibs.Implementation;
 using Guru99Application.Pages;
+using Microsoft.Extensions.Configuration;
 using NUnit.Framework;
 
 namespace Guru99Tests.tests
@@ -12,13 +14,25 @@ namespace Guru99Tests.tests
     {
         public CommonDriver CmnDriver;
         public LoginPage loginPage;
-        string url = "http://demo.guru99.com/v4";
+        private IConfigurationRoot _configuration;
+        string _url;
+        string _currentProjectDirectory;
+
+        [OneTimeSetUp]
+        public void PreSetup()
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            _currentProjectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            _configuration = new ConfigurationBuilder().AddJsonFile(_currentProjectDirectory + "/config/appSetting.json").Build();
+        }
 
         [SetUp]
         public void Setup()
         {
-            CmnDriver = new CommonDriver("chrome");
-            CmnDriver.NavigateToFirstUrl(url);
+            string browserType = _configuration["browserType"];
+            CmnDriver = new CommonDriver(browserType);
+            _url = _configuration["baseUrl"];
+            CmnDriver.NavigateToFirstUrl(_url);
             loginPage = new LoginPage(CmnDriver.Driver);
         }
 
