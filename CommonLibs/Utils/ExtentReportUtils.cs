82ab7aee38855fc904f50085ad0ec852a9626c71
DilanOjeda <dilanojeda.engineer@gmail.com>
2022-09-26 23:48:09 -0400

    Generaing logs and reports using extentreport

---
+++ CommonLibs/Utils/ExtentReportUtils.cs
@@ -0,0 +1,29 @@
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+
+namespace CommonLibs.Utils
+{
+    public class ExtentReportUtils
+    {
+        private ExtentHtmlReporter _extentHtmlReporter;
+        private ExtentReports _extentReports;
+        private ExtentTest _extentTest;
+
+        public ExtentReportUtils(string htmlReportFileName)
+        {
+            _extentHtmlReporter = new ExtentHtmlReporter(htmlReportFileName);
+            _extentReports = new ExtentReports();
+            _extentReports.AttachReporter(_extentHtmlReporter);
+        }
+
+        public void CreateATestCase(string testCaseName)
+        {
+            _extentTest = _extentReports.CreateTest(testCaseName);
+        }
+
+        public void AddTestLog(Status status, string comment)
+        {
+            _extentTest.Log(status, comment);
+        }
+    }
+}
